d89a7fc36b715dd868a2bb79237e8ebcd02b1d75
Daniel <ramos.danielferreira@gmail.com>
2020-02-25 18:51:32 -0300

    Removido json do coverage

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    testEnvironment: 'node',
     collectCoverage: true,
-    coverageReporters: ['json', 'html'],
+    coverageReporters: ['html'],
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testPathIgnorePatterns: ['<rootDir>/dist'],
