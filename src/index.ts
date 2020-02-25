050a896bee877d4a1646cd5519dc7658a67c1a8c
Daniel <ramos.danielferreira@gmail.com>
2020-02-25 19:54:11 -0300

    Idea files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 /* eslint-env node, mocha */
 import * as express from 'express';
+import { helloTest } from './hello';
 
 const PORT = process.env.PORT ?? 8080;
 const app = express();
 
 export const helloWorld = (req: express.Request, res: express.Response) => {
     res.json({
-        message: 'Mensagem de exemplo',
+        message: helloTest() ? 'Mensagem de exemplo' : 'Testee',
     });
 };
 
